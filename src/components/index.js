c5aa3993760e40b0979fb8973db3526db7e85c4e
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-11-05 18:15:48 +0300

    init

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import EmojiInput from './EmojiInput';
 import ModalConfirm from './ModalConfirm';
 import Table from './Table';
-import Header from './Header';
 
 export {
   EmojiInput,
   ModalConfirm,
-  Header,
   Table,
 };
