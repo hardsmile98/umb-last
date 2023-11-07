0e634755da8a137c280bfba0d22f8e2d2e91bd5e
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-11-07 20:44:11 +0300

    feat

--- src/pages/Dashboard/Shop/Datas/Purchase/SetAsNoffoundModal/index.jsx
+++ src/pages/Dashboard/Shop/Datas/Purchase/SetAsNoffoundModal/index.jsx
@@ -1,50 +1,51 @@
-/* eslint-disable react/destructuring-assignment */
-import React, { Component } from 'react';
-import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+import React, { useState } from 'react';
 import { toast } from 'react-toastify';
-import { request, getLocales } from 'utils';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+import { getLocales, request } from 'utils';
 
-class SetAsNoffoundModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      record: 0,
-      fine: 0,
-      fineInd: 0,
-      loading: false,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.sendData = this.sendData.bind(this);
-  }
+function SetAsNoffoundModal({
+  modal,
+  toggle,
+  ownerAdd,
+  currency,
+  shop,
+  purchase,
+  getData,
+}) {
+  const [isLoading, setLoading] = useState(false);
 
-  handleChange(e) {
+  const [data, setData] = useState({
+    record: 0,
+    fine: 0,
+    fineInd: 0,
+  });
+
+  const handleChange = (e) => {
     const value = e.target[e.target.type === 'checkbox'
       ? 'checked'
       : 'value'];
     const { name } = e.target;
 
-    this.setState({
+    setData((prev) => ({
+      ...prev,
       [name]: value,
-    });
-  }
+    }));
+  };
 
-  sendData() {
-    this.setState({
-      loading: true,
-    });
-    const data = {
+  const sendData = () => {
+    setLoading(true);
+
+    const body = {
       api: 'user',
       body: {
         data: {
           section: 'shop',
           type: 'datas',
           subtype: 'purchases',
-          shop: this.props.shop,
+          shop,
           action: 'setNotFound',
-          id: this.props.purchase,
-          record: this.state.record,
-          fine: this.state.fine,
-          fineInd: this.state.fineInd,
+          id: purchase,
+          ...data,
         },
         action: 'shops',
       },
@@ -53,123 +54,125 @@ class SetAsNoffoundModal extends Component {
       },
     };
 
-    request(data, (response) => {
-      if (response.status === 200) {
-        if (response.data.success) {
-          toast.success(response.data.message);
-          this.props.getData();
-          this.props.toggle();
-        } else {
-          this.setState({
-            loading: false,
-          });
-          toast.error(response.data.message);
-        }
-      } else {
+    request(body, (response) => {
+      if (response.status !== 200) {
         toast.error('Сервер недоступен');
+        return;
       }
-    });
-  }
 
-  render() {
-    return (
-      <Modal size="md" isOpen={this.props.modal} toggle={this.props.toggle}>
-        <div className="modal-header text-center">
-          <h4 className="modal-title font-m">
-            {getLocales('Опции для ненахода')}
-          </h4>
+      if (!response.data.success) {
+        setLoading(false);
+        toast.error(response.data.message);
+        return;
+      }
+
+      toast.success(response.data.message);
+      getData();
+      toggle();
+    });
+  };
+
+  return (
+    <Modal
+      size="md"
+      isOpen={modal}
+      toggle={toggle}
+    >
+      <div className="modal-header text-center">
+        <h4 className="modal-title font-m">
+          {getLocales('Опции для ненахода')}
+        </h4>
+      </div>
+
+      <ModalBody>
+        <div className="form-group">
+          <label className="form-control-label font-m">
+            {getLocales('Учитывать покупку в общем обороте')}
+          </label>
+          <select
+            disabled={isLoading}
+            value={data.record}
+            onChange={handleChange}
+            name="record"
+            className="form-control"
+          >
+            <option value="0">{getLocales('Нет')}</option>
+            <option value="1">{getLocales('Да')}</option>
+          </select>
         </div>
 
-        <ModalBody>
+        {!ownerAdd && (
           <div className="form-group">
             <label className="form-control-label font-m">
-              {getLocales('Учитывать покупку в общем обороте')}
+              {getLocales('Оштрафовать курьера')}
             </label>
             <select
-              disabled={this.state.loading}
-              value={this.state.record}
-              onChange={this.handleChange}
-              name="record"
+              disabled={isLoading}
+              value={data.fine}
+              onChange={handleChange}
+              name="fine"
               className="form-control"
             >
               <option value="0">{getLocales('Нет')}</option>
               <option value="1">{getLocales('Да')}</option>
+              <option value="2">{getLocales('Да, но по отдельному тарифу')}</option>
             </select>
           </div>
+        )}
 
-          {!this.props.ownerAdd && (
-            <div className="form-group">
-              <label className="form-control-label font-m">
-                {getLocales('Оштрафовать курьера')}
-              </label>
-              <select
-                disabled={this.state.loading}
-                value={this.state.fine}
-                onChange={this.handleChange}
-                name="fine"
+        {String(data.fine) === '2' && (
+          <div className="form-group">
+            <label className="form-control-label font-m">
+              {getLocales('Штраф для курьера')}
+            </label>
+            <div className="input-group">
+              <input
+                disabled={isLoading}
+                value={data.fineInd}
+                name="fineInd"
+                onChange={handleChange}
                 className="form-control"
-              >
-                <option value="0">{getLocales('Нет')}</option>
-                <option value="1">{getLocales('Да')}</option>
-                <option value="2">{getLocales('Да, но по отдельному тарифу')}</option>
-              </select>
-            </div>
-          )}
-
-          {String(this.state.fine) === '2' && (
-            <div className="form-group">
-              <label className="form-control-label font-m">
-                {getLocales('Штраф для курьера')}
-              </label>
-              <div className="input-group">
-                <input
-                  disabled={this.state.loading}
-                  value={this.state.fineInd}
-                  name="fineInd"
-                  onChange={this.handleChange}
-                  className="form-control"
-                />
-                <span className="input-group-text">
-                  {this.props.currency}
-                </span>
-              </div>
-            </div>
-          )}
-        </ModalBody>
-
-        <ModalFooter>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-4">
-                <button
-                  type="button"
-                  value={getLocales('Закрыть')}
-                  className="btn btn-secondary font-m auth-btn"
-                  onClick={this.props.toggle}
-                >
-                  {getLocales('Закрыть')}
-                </button>
-              </div>
-
-              <div className="col-lg-8">
-                <button
-                  type="button"
-                  value="Отметить"
-                  onClick={this.sendData}
-                  disabled={this.props.loading}
-                  className="btn btn-primary font-m auth-btn"
-                >
-                  {this.props.loading
-                    ? getLocales('Загрузка...')
-                    : getLocales('Отметить')}
-                </button>
-              </div>
+              />
+              <span className="input-group-text">
+                {currency}
+              </span>
             </div>
           </div>
-        </ModalFooter>
-      </Modal>
-    );
-  }
+        )}
+      </ModalBody>
+
+      <ModalFooter>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-4">
+              <button
+                type="button"
+                value={getLocales('Закрыть')}
+                className="btn btn-secondary font-m auth-btn"
+                onClick={toggle}
+              >
+                {getLocales('Закрыть')}
+              </button>
+            </div>
+
+            <div className="col-lg-8">
+              <button
+                type="button"
+                value="Отметить"
+                onClick={sendData}
+                disabled={isLoading}
+                className="btn btn-primary font-m auth-btn"
+              >
+                {isLoading
+                  ? getLocales('Загрузка...')
+                  : getLocales('Отметить')}
+              </button>
+            </div>
+          </div>
+        </div>
+      </ModalFooter>
+    </Modal>
+  );
 }
 
 export default SetAsNoffoundModal;
