6a76e55d326bdf65fff98eccfcffcd8fdf5f30e7
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-11-08 18:35:49 +0300

    feat

--- src/pages/Dashboard/Shop/Statistics/ProfitModal/index.jsx
+++ src/pages/Dashboard/Shop/Statistics/ProfitModal/index.jsx
@@ -1,226 +1,223 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/no-deprecated */
-import React, { Component } from 'react';
-import { Modal, ModalBody, ModalFooter } from 'reactstrap';
+import React, { useCallback, useEffect, useState } from 'react';
 import moment from 'moment';
+import { Modal, ModalBody, ModalFooter } from 'reactstrap';
 import { getLocales } from 'utils';
 
-class ProfitModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dateFrom: moment.unix(new Date(Date.now() - 2592000000)
-        .setHours(0, 0, 0, 0) / 1000).format('YYYY-MM-DD'),
-      dateTo: moment.unix(new Date(Date.now() + 86400000)
-        .setHours(0, 0, 0, 0) / 1000).format('YYYY-MM-DD'),
-      profit: {},
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.getProfit = this.getProfit.bind(this);
-  }
+function ProfitModal({
+  modal,
+  toggle,
+  currency,
+  purchases,
+  products,
+  subproducts,
+}) {
+  const [filter, setFilter] = useState({
+    dateFrom: moment.unix(new Date(Date.now() - 2592000000)
+      .setHours(0, 0, 0, 0) / 1000).format('YYYY-MM-DD'),
+    dateTo: moment.unix(new Date(Date.now() + 86400000)
+      .setHours(0, 0, 0, 0) / 1000).format('YYYY-MM-DD'),
+  });
+  const [profit, setProfit] = useState({});
 
-  componentDidMount() {
-    this.getProfit();
-  }
+  const getProfit = useCallback(() => {
+    const formattedData = {};
 
-  componentWillReceiveProps() {
-    this.getProfit();
-  }
-
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value,
-    }, () => {
-      this.getProfit();
-    });
-  }
-
-  getProfit() {
-    const res = {};
-
-    this.props.purchases.map((purchase) => {
-      if (+purchase.closed >= +new Date(this.state.dateFrom)
-       && +purchase.closed < +new Date(this.state.dateTo)) {
+    purchases.forEach((purchase) => {
+      if (+purchase.closed >= +new Date(filter.dateFrom)
+        && +purchase.closed < +new Date(filter.dateTo)) {
         if (!purchase.subproduct) {
-          this.props.products.map((item) => {
+          products.forEach((item) => {
             if (item.name === purchase.product) {
-              if (res[purchase.category]) {
-                res[purchase.category].turnover += +purchase.sum;
-                res[purchase.category].courier += +item.bonus;
-                res[purchase.category].seb += 0;
-                res[purchase.category].prefer += (+purchase.sum - +item.bonus - 0);
-              } else {
-                res[purchase.category] = {
+              const category = formattedData[purchase.category];
+
+              formattedData[purchase.category] = category
+                ? {
+                  ...category,
+                  turnover: category.turnover + (+purchase.sum),
+                  courier: category.courier + (+item.bonus),
+                  prefer: category.prefer + (+purchase.sum - +item.bonus - 0),
+                }
+                : {
                   turnover: +purchase.sum,
                   courier: +item.bonus,
                   seb: 0,
                   prefer: (+purchase.sum - +item.bonus - 0),
                 };
-              }
             }
           });
         } else {
-          this.props.subproducts.map((item) => {
+          subproducts.forEach((item) => {
             if (item.name === purchase.subproduct) {
-              if (res[purchase.category]) {
-                res[purchase.category].turnover += +purchase.sum;
-                res[purchase.category].courier += +item.bonus;
-                res[purchase.category].seb += +item.sum;
-                res[purchase.category].prefer += (+purchase.sum - +item.bonus - +item.sum);
-              } else {
-                res[purchase.category] = {
+              const category = formattedData[purchase.category];
+
+              formattedData[purchase.category] = category
+                ? {
+                  ...category,
+                  turnover: category.turnover + (+purchase.sum),
+                  courier: category.courier + (+item.bonus),
+                  seb: category.seb + (+item.sum),
+                  prefer: category.prefer + (+purchase.sum - +item.bonus - +item.sum),
+                }
+                : {
                   turnover: +purchase.sum,
                   courier: +item.bonus,
                   seb: +item.sum,
                   prefer: (+purchase.sum - +item.bonus - +item.sum),
                 };
-              }
             }
           });
         }
       }
     });
 
-    this.setState({
-      profit: res,
-    });
-  }
+    setProfit(formattedData);
+  }, [filter, products, subproducts, purchases]);
 
-  render() {
-    return (
-      <Modal
-        size="xl"
-        isOpen={this.props.modal}
-        toggle={this.props.toggle}
-      >
-        <div className="modal-header text-center">
-          <h4 className="modal-title font-m">
-            {getLocales('Рассчет прибыли')}
-          </h4>
-        </div>
+  useEffect(() => {
+    getProfit();
+  }, [getProfit]);
 
-        <ModalBody>
-          <div className="row">
-            <div className="col-lg-6">
-              <div className="form-group">
-                <label className="form-control-label font-m">
-                  {getLocales('От')}
-                </label>
-                <input
-                  type="date"
-                  onChange={this.handleChange}
-                  value={this.state.dateFrom}
-                  name="dateFrom"
-                  className="form-control"
-                />
-              </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFilter((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  return (
+    <Modal
+      size="xl"
+      isOpen={modal}
+      toggle={toggle}
+    >
+      <div className="modal-header text-center">
+        <h4 className="modal-title font-m">
+          {getLocales('Рассчет прибыли')}
+        </h4>
+      </div>
+
+      <ModalBody>
+        <div className="row">
+          <div className="col-lg-6">
+            <div className="form-group">
+              <label className="form-control-label font-m">
+                {getLocales('От')}
+              </label>
+              <input
+                type="date"
+                onChange={handleChange}
+                value={filter.dateFrom}
+                name="dateFrom"
+                className="form-control"
+              />
             </div>
+          </div>
 
-            <div className="col-lg-6">
-              <div className="form-group">
-                <label className="form-control-label font-m">
-                  {getLocales('До')}
-                </label>
-                <input
-                  type="date"
-                  onChange={this.handleChange}
-                  value={this.state.dateTo}
-                  name="dateTo"
-                  className="form-control"
-                />
-              </div>
+          <div className="col-lg-6">
+            <div className="form-group">
+              <label className="form-control-label font-m">
+                {getLocales('До')}
+              </label>
+              <input
+                type="date"
+                onChange={handleChange}
+                value={filter.dateTo}
+                name="dateTo"
+                className="form-control"
+              />
             </div>
+          </div>
 
-            <div className="col-lg-12">
-              <div className="avatar-block notice no-margin">
-                <h3 className="font-m">
-                  {getLocales('Статистика по городам')}
-                </h3>
+          <div className="col-lg-12">
+            <div className="avatar-block notice no-margin">
+              <h3 className="font-m">
+                {getLocales('Статистика по городам')}
+              </h3>
 
-                {Object.keys(this.state.profit).length > 0
-                  ? (
-                    <div>
-                      <div className="avatar-block font-m">
+              {Object.keys(profit).length > 0
+                ? (
+                  <div>
+                    <div className="avatar-block font-m">
+                      <div className="row">
+                        <div className="col-lg-2">
+                          <b>Город</b>
+                        </div>
+                        <div className="col-lg-2 text-center">
+                          <b>Оборот</b>
+                        </div>
+                        <div className="col-lg-2 text-center">
+                          <b>З/п курьерам</b>
+                        </div>
+                        <div className="col-lg-3 text-center">
+                          <b>Себестоимость товара</b>
+                        </div>
+                        <div className="col-lg-2 text-center">
+                          <b>Чистая прибыль</b>
+                        </div>
+                      </div>
+                    </div>
+
+                    {Object.keys(profit).map((key) => (
+                      <div className="avatar-block font-m" key={key}>
                         <div className="row">
                           <div className="col-lg-2">
-                            <b>Город</b>
+                            {key}
                           </div>
                           <div className="col-lg-2 text-center">
-                            <b>Оборот</b>
+                            {Math.round(profit[key].turnover)}
+                            {' '}
+                            {currency}
                           </div>
                           <div className="col-lg-2 text-center">
-                            <b>З/п курьерам</b>
+                            {Math.round(profit[key].courier)}
+                            {' '}
+                            {currency}
                           </div>
                           <div className="col-lg-3 text-center">
-                            <b>Себестоимость товара</b>
+                            {Math.round(profit[key].seb)}
+                            {' '}
+                            {currency}
                           </div>
                           <div className="col-lg-2 text-center">
-                            <b>Чистая прибыль</b>
+                            {Math.round(profit[key].prefer)}
+                            {' '}
+                            {currency}
                           </div>
                         </div>
                       </div>
-
-                      {Object.keys(this.state.profit).map((key) => (
-                        <div className="avatar-block font-m" key={key}>
-                          <div className="row">
-                            <div className="col-lg-2">
-                              {key}
-                            </div>
-                            <div className="col-lg-2 text-center">
-                              {Math.round(this.state.profit[key].turnover)}
-                              {' '}
-                              {this.props.currency}
-                            </div>
-                            <div className="col-lg-2 text-center">
-                              {Math.round(this.state.profit[key].courier)}
-                              {' '}
-                              {this.props.currency}
-                            </div>
-                            <div className="col-lg-3 text-center">
-                              {Math.round(this.state.profit[key].seb)}
-                              {' '}
-                              {this.props.currency}
-                            </div>
-                            <div className="col-lg-2 text-center">
-                              {Math.round(this.state.profit[key].prefer)}
-                              {' '}
-                              {this.props.currency}
-                            </div>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  )
-                  : (
-                    <div className="text-center font-m">
-                      Не найдено
-                    </div>
-                  )}
-              </div>
+                    ))}
+                  </div>
+                )
+                : (
+                  <div className="text-center font-m">
+                    Не найдено
+                  </div>
+                )}
             </div>
           </div>
-        </ModalBody>
+        </div>
+      </ModalBody>
 
-        <ModalFooter>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-lg-12">
-                <button
-                  type="button"
-                  value={getLocales('Закрыть')}
-                  className="btn btn-secondary font-m auth-btn"
-                  onClick={this.props.toggle}
-                >
-                  {getLocales('Закрыть')}
-                </button>
-              </div>
+      <ModalFooter>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12">
+              <button
+                type="button"
+                value={getLocales('Закрыть')}
+                className="btn btn-secondary font-m auth-btn"
+                onClick={toggle}
+              >
+                {getLocales('Закрыть')}
+              </button>
             </div>
           </div>
-        </ModalFooter>
-      </Modal>
-    );
-  }
+        </div>
+      </ModalFooter>
+    </Modal>
+  );
 }
 
 export default ProfitModal;
