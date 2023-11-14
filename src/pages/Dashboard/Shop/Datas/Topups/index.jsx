9d29d8b5d44787a1b4b124bad65f3b109bac9660
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-11-14 23:35:29 +0300

    feat

--- src/pages/Dashboard/Shop/Datas/Topups/index.jsx
+++ src/pages/Dashboard/Shop/Datas/Topups/index.jsx
@@ -1,9 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React, { useCallback, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import moment from 'moment';
 import { toast } from 'react-toastify';
 import { getLocales, request } from 'utils';
 import { Table } from 'components';
+
+const getType = (reason) => {
+  switch (reason) {
+    case 'topup':
+      return getLocales('Пополнение');
+    case 'overpay':
+      return getLocales('Переплата');
+    case 'smallPay':
+      return getLocales('Недоплата');
+    default:
+      return null;
+  }
+};
 
 function Topups() {
   const { shopId } = useParams();
@@ -16,8 +30,8 @@ function Topups() {
     users: [],
   });
   const [filter, setFilter] = useState({
-    typeSort: 'all',
-    paymentSort: 'all',
+    reasonSort: '',
+    paymentSort: '',
     dateFrom: '',
     dateTo: '',
   });
@@ -29,6 +43,56 @@ function Topups() {
     ...prev,
     [e.target.name]: e.target.value,
   }));
+
+  const getPayments = (paymentsData) => {
+    if (paymentsData.length) {
+      const formatted = paymentsData.reduce((acc, cur) => {
+        const { type } = cur;
+
+        if (!acc.includes(type)) {
+          acc.push(type);
+        }
+
+        return acc;
+      }, []);
+
+      setPaymenst(formatted);
+    }
+  };
+
+  const prepareItems = useCallback(() => {
+    let copy = [...data.topups];
+
+    if (filter.paymentSort !== '') {
+      copy = copy.filter((purchase) => purchase.type === filter.paymentSort);
+    }
+
+    if (filter.reasonSort !== '') {
+      copy = copy.filter((purchase) => purchase.reason === filter.reasonSort);
+    }
+
+    if (filter.dateFrom !== '') {
+      copy = copy.filter((purchase) => +purchase.closed >= +new Date(filter.dateFrom));
+    }
+
+    if (filter.dateTo !== '') {
+      copy = copy.filter((purchase) => +purchase.closed <= +new Date(filter.dateTo));
+    }
+
+    const formattedItems = copy.map((item) => ({
+      id: item.id,
+      reason: getType(item.reason),
+      sum: `${item.sum} ${data.currency}`,
+      type: item.type,
+      date: moment.unix(item.closed / 1000).format('LLL'),
+    }));
+
+    setItems(formattedItems);
+  }, [filter, data]);
+
+  useEffect(() => {
+    prepareItems();
+  }, [prepareItems]);
 
   const getData = useCallback(() => {
     // setLoading(true);
@@ -86,6 +150,20 @@ function Topups() {
               toUser: 0.0001,
               purchase: 0,
             },
+            {
+              id: 1325,
+              reason: 'overpay',
+              user: 73,
+              sum: 850,
+              created: '1697319518048',
+              closed: '1697319903749',
+              status: 1,
+              data: '{"category":"3","subcategory":0,"product":"2","subproduct":0,"user":73,"typeofklad":0,"sum":850,"method":"LTC","presale":true}',
+              type: 'BTC',
+              deposit: 593690,
+              toUser: 0.0001,
+              purchase: 0,
+            },
           ],
           currency: 'RUB',
           users: [
@@ -114,6 +192,7 @@ function Topups() {
       },
     };
 
+    getPayments(response.data.data.topups);
     setData(response.data.data);
   }, []);
 
@@ -126,7 +205,32 @@ function Topups() {
       title: 'ID', dataIndex: 'id', key: 'id', sort: true,
     },
     {
-      title: getLocales('Город'), dataIndex: 'category', key: 'category', sort: true,
+      title: getLocales('Тип'), dataIndex: 'reason', key: 'reason', sort: true,
+    },
+    {
+      title: getLocales('Сумма'), dataIndex: 'sum', key: 'sum', sort: true,
+    },
+    {
+      title: getLocales('Способ оплаты'), dataIndex: 'type', key: 'type', sort: true,
+    },
+    {
+      title: getLocales('Дата'), dataIndex: 'date', key: 'date', sort: true,
+    },
+    {
+      title: getLocales('Действие'),
+      dataIndex: 'name',
+      itemClassName: 'text-center',
+      headerClassName: 'text-center',
+      key: 'operations',
+      render: (_e, item) => (
+        <NavLink
+          to={`/dashboard/shops/${shopId}/datas/topups/${item.id}`}
+        >
+          <button type="button" className="btn btn-secondary font-m">
+            {getLocales('Подробнее')}
+          </button>
+        </NavLink>
+      ),
     },
   ];
 
@@ -160,12 +264,12 @@ function Topups() {
                           </label>
                           <select
                             disabled={isLoading}
-                            value={filter.typeSort}
+                            value={filter.reasonSort}
                             onChange={onChangeFilter}
-                            name="typeSort"
+                            name="reasonSort"
                             className="form-control"
                           >
-                            <option value="all">{getLocales('Все')}</option>
+                            <option value="">{getLocales('Все')}</option>
                             <option value="topup">{getLocales('Пополнение')}</option>
                             <option value="overpay">{getLocales('Переплата')}</option>
                             <option value="smallPay">{getLocales('Недоплата')}</option>
@@ -185,7 +289,7 @@ function Topups() {
                             name="paymentSort"
                             className="form-control"
                           >
-                            <option value="all">{getLocales('Все')}</option>
+                            <option value="">{getLocales('Все')}</option>
                             {payments.map((item) => (
                               <option value={item} key={item}>
                                 {item}
